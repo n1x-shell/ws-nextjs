3a25f026ca09003086bd40a5e4f96b0292fcd4e9
n1x-shell <jnixxxo@gmail.com>
2026-02-14 23:10:30 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'text/html; charset=utf-8',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
