953f49c5a6f39377a1b5a5c3aef2e39e698f78da
Nix <jnixxxo@gmail.com>
2026-02-24 03:32:38 -0800

    Add files via upload

--- components/neural/NeuralCore.tsx
+++ components/neural/NeuralCore.tsx
@@ -3,10 +3,17 @@
 import { NeuralProvider } from '@/contexts/NeuralContext';
 import SignalLayer from './SignalLayer';
 import InterfaceLayer from './InterfaceLayer';
+import { useShellPresence } from '@/lib/useShellPresence';
+
+function ShellPresenceMount() {
+  useShellPresence();
+  return null;
+}
 
 export default function NeuralCore() {
   return (
     <NeuralProvider>
+      <ShellPresenceMount />
       <SignalLayer />
       <InterfaceLayer />
     </NeuralProvider>
